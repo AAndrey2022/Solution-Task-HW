131e97cda54d52024ffaef1499e68670e9d79ab4
andrey <groza001@yandex.ru>
2023-01-18 20:11:02 +0300

    Add solution Task002

--- Task002/Program.cs
+++ Task002/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/// Напишите программу, которая:
+// 1. На вход принимает два числа 
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+// 2.выдаёт, какое число большее, а какое меньшее.
+
+Console.Write("Введите целое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите целое число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 > num2)
+{
+    Console.WriteLine($"Число {num1} Max");
+}
+if (num1 < num2)
+{
+  Console.WriteLine($"Число {num2} Max");  
+}
+if (num1 == num2)
+{
+ Console.WriteLine($"Числа равны");   
+}
