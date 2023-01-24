2451602a87ccfb321bbf2830b8fdfe75efc3abe3
andrey <groza001@yandex.ru>
2023-01-24 13:38:41 +0300

    Add solution Task023

---
+++ Task023/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая:
+// - принимает на вход число (N) и
+// - выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+Console.Write("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void CubeDigitals(int num)
+{
+    int count = 1;
+    while (count <= num)
+    {
+        Console.WriteLine($" {count, 3}:  {count * count * count, 5}");
+        count++;
+    }
+}
+CubeDigitals(number);
