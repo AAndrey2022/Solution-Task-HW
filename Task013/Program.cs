4dbc171c552461185304c6b3a02efe1ac42b7a0e
andrey <groza001@yandex.ru>
2023-01-24 11:34:02 +0300

    Add solution Task013

--- Task013/Program.cs
+++ Task013/Program.cs
@@ -10,35 +10,21 @@
 // 32679 -> 6
 Console.Write("Введите число: ");
 int num = Convert.ToInt32(Console.ReadLine());
-int count = 0;
-// int num3=Convert.ToInt32(Console.ReadLine());;
+int num1 = 1;
+int num2 = 1;
+
 if (num <= 100)
     Console.Write("Третьей цмфры нет");
-   else
-   Console.Write($"Третья цифра-> {num % 10}");
- 
- if (num>=1000)
- while (num <= 3)
-    {
-       count=num % 10;
-       
-    }
-    Console.Write($"Третья цифра-> {count }");
+else
+    Console.Write($"Третья цифра-> {num % 10}");
 
-    
-
-// else
-// Console.Write("Некорректное значение");
-
-// int outputthirddigit = OutputThirdDigit(num);
-// Console.Write($"Третье число -> {outputthirddigit} ");
-// int OutputThirdDigit(int number)
-// {
-//     if (number<=100)
-//    return number ("Третьей цифры нет");
-//    else
-//    if (number>=1000)
-//     while(number)
-// int result = number % 10;
-// return result;
-// }
+if (num > 999)
+{
+    num1 = num / 10 % 10;
+    Console.Write($"Третья цифра-> {num1}");
+}
+if (num > 10000)
+{
+    num2 = num / 100 % 10;
+    Console.Write($"Третья цифра-> {num2}");
+}
