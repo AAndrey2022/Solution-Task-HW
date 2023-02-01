16683bd848613825723873499b71b1de986cb96d
andrey <groza001@yandex.ru>
2023-02-01 19:46:52 +0300

    Add solution Task034

---
+++ Task034/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+int[] arr = CreateArrRndEvenDigitInt(4);
+PrintArray(arr);
+int showEven = ShowEvenNumberArray(arr);
+Console.Write($" -> {showEven}");
+
+int[] CreateArrRndEvenDigitInt(int size)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(100, 1000);
+    }
+    return array;
+}
+
+int ShowEvenNumberArray(int[] arr)
+{
+    int res = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            res = res + 1;
+        }
+
+    }
+    return res;
+}
+
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.Write("]");
+}
+
+// void PrintArray(int[] arr)
+// {
+//     Console.Write("[");
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         if(i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+//         else Console.Write($"{arr[i]}");
+//     }
+//     Console.Write("]");
+// }
